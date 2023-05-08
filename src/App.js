188c0abf8ba9f86cd1d51deacd8e45a40d1ef5de
Jitu <jitenmohantyaz@gmail.com>
2023-05-08 08:42:12 +0530

    Dark Mode Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForms from "./components/TextForms";
 
 function App() {
+  const[mode,setMode] = useState('light');
+  const toggleMode= ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
   return (
     <>
-      <Navbar tittle="TextUtils" about="About Us" />
+      <Navbar tittle="TextUtils" about="About Us" mode={mode} toggleMode={toggleMode}/>
       <div className="container">
-        <TextForms heading="Add text to analyze.." />
+        <TextForms heading="Add text to analyze.."  mode={mode}/>
+        {/* <About/> */}
       </div>
     </>
   );
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,11 +1,37 @@
 import React,{useState} from "react";
 //Use state use to create state variable.
 export default function TextForms(props) {
+  const [text,setText] = useState('');
     const handleUpClick = () => {
         // console.log("UpperCase was clicked"+text);
         let newText=text.toUpperCase();
         setText(newText);
     }
+
+    // const [copySuccess, setCopySuccess] = useState(null);
+    const copyClick = async copyMe => {
+       try {
+           await navigator.clipboard.writeText(copyMe);
+           window.alert('Massage copied');
+          //  setText('Copied!');
+       } 
+       catch (err) {
+           setText('Failed to copy!');
+       }
+    };
+
+
+    const clearClick = () => {
+      // console.log("UpperCase was clicked"+text);
+      let newText='';
+      setText(newText);
+  }
+
+  const removeExtraSpace = () => {
+    // console.log("UpperCase was clicked"+text);
+    let newText=text.split(/[ ]+/);
+    setText(newText.join(" "));
+}
     
     const handleLoClick = () => {
         // console.log("UpperCase was clicked"+text);
@@ -18,26 +44,31 @@ export default function TextForms(props) {
         // console.log("on Change");
         setText(event.target.value);
     }
-    const [text,setText] = useState('Enter text here ');
 
   return (
     <>
-    <div>
+    <div style={{color:props.mode  === 'dark'?'white':'black'}}>
       <h1>{props.heading}</h1>
-      <div className="my-3">
-        <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
+      <div className=" container my-3" >
+        <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode  === 'dark'?'grey':'white',color:props.mode  === 'dark'?'white':'black'}}></textarea>
 
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
-      <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+      <button className="btn btn-secondary mx-2" onClick={handleLoClick}>Convert to LowerCase</button>
+      <button className="btn btn-danger mx-2" onClick={clearClick}>Clear all</button>
+      <button className="btn btn-success mx-2" onClick={removeExtraSpace}>removeExtraSpace</button>
+      <button className="btn btn-success mx-2" onClick={(e) => copyClick(text)}>Copy to Clipboard</button> 
+   
     </div>
-    <div className="container my-3">
+    
+    
+    <div className="container my-3" style={{color:props.mode  === 'dark'?'white':'black'}}>
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length} Words and {text.length} Character</p>
         <p>{0.008 * text.split(" ").length} Minute to read</p>
 
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter something above text box to Preview It"}</p>
     </div>
     </>
   );
